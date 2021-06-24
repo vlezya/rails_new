73b495a5a096174c6e07a37a28f21aab6b5440c8
StasV <vlezyashop@gmail.com>
2021-06-24 17:25:34 +0300

    instagram_project

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ gem 'bootstrap', '~> 4.1.1'
 gem 'jquery-rails'
 gem 'font-awesome-rails'
 gem 'devise'
+gem 'kaminari'
+gem 'mini_magick'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.6'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'home/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
-  resources :users, only: [:show]
+
+  devise_for :users
+  resources :users, only: [:show, :edit, :update]
+  resources :posts, only: [:new, :create, :show, :destroy]
+  get 'search' => 'search#index'
 end
